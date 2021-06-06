7d64df1ec4b80c1fcb7415a9f8ab9a94da959608
dev-Lesser <dev.jongmoon@gmail.com>
2021-06-07 08:22:35 +0900

    :construction: Edit chart data in condition

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,18 @@
 
 <template>
     <v-footer padless color="blue-grey darken-3" dark>
-        <v-col class="text-center" cols="12">
-            
+        <v-col class="text-center" cols="12" style="display:flex;">
+            <div>
+                <v-btn
+                    class="ma-2"
+                    dark
+                    href="https://github.com/dev-Lesser"
+                    icon
+                >
+                    <v-icon >mdi-github-circle</v-icon>
+                </v-btn>
+            </div>
+            <v-spacer />
             <div style="font-size:13px;display:flex;justify-content:flex-end;align-items:center;">
                 &#169; {{ new Date().getFullYear() }}.
                 <v-chip outlined x-small class="ma-1" color="red" href="https://github.com/dev-Lesser">dev-Lesser</v-chip> All Rights Reserved.
